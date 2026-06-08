5b3f56d0f01af9c4022d3f00ee4b7063050f446b
gitson11 <109638254+gitson11@users.noreply.github.com>
2026-06-08 01:31:33 -0300

    Atualiza log inicial para Ordem Liturgica

--- culto_louveapp_manager/main.py
+++ culto_louveapp_manager/main.py
@@ -1,3 +1,4 @@
+from src.config import APP_NAME
 from src.database import initialize_database
 from src.gui import CultoLouveAppManager
 from src.logger import get_logger, setup_logging
@@ -6,7 +7,7 @@ from src.logger import get_logger, setup_logging
 def main() -> None:
     setup_logging()
     logger = get_logger(__name__)
-    logger.info("Abrindo Culto LouveApp Manager")
+    logger.info("Abrindo %s", APP_NAME)
     initialize_database()
     app = CultoLouveAppManager()
     app.mainloop()
