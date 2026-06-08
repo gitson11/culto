af5549d1ee0c714e16a9d1b4799f768d5a0f206b
gitson11 <109638254+gitson11@users.noreply.github.com>
2026-06-08 09:52:30 -0300

    Adiciona compatibilidade para import de scale_models

---
+++ culto_louveapp_manager/src/scale_models.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from src.schedule_models import (
+    SCALE_MODEL_DB_FIELDS,
+    SCALE_MODEL_FIELDS,
+    SCALE_MODEL_SLOT_DB_FIELDS,
+    SCALE_MODEL_SLOT_FIELDS,
+    ScaleModel,
+    ScaleModelSlot,
+)
+
+__all__ = [
+    "SCALE_MODEL_DB_FIELDS",
+    "SCALE_MODEL_FIELDS",
+    "SCALE_MODEL_SLOT_DB_FIELDS",
+    "SCALE_MODEL_SLOT_FIELDS",
+    "ScaleModel",
+    "ScaleModelSlot",
+]
